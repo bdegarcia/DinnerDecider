9f4a747efbbf92de7b319f4a4a013c21c8f24f6b
Ben de Garcia <degarcia.ben@gmail.com>
2020-06-23 21:14:49 -0500

    minor fixes

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A website that provides a user with a list of possible cooking recipies based on
 <img src="assets/project_snips/snip_main.jpg">
 
 Here is a deployed website that has the ability to input ingredients that the user has, or a craving the user might have, and output a recipe based on those, as well as time allotted. 
+
+This website was build using Bulma HTML framework, and JQuery Javascript library
--- assets/javascript.js
+++ assets/javascript.js
@@ -1,51 +1,52 @@
-var edamamURL = "https://api.edamam.com/search?q="
-var edamAppID = "578c5ca1"
-var edamKey = "8bca4f7e2dc388b15b67b50c90a7cf77"
+var edamamURL = "https://api.edamam.com/search?q=";
+var edamAppID = "578c5ca1";
+var edamKey = "8bca4f7e2dc388b15b67b50c90a7cf77";
 
 
 $("#drink-button").on("click", function(event){
-    event.preventDefault()
-    
+    event.preventDefault();
+    //This is the drink portion of the website. Ray figured out the API call, and the info that returns
     var url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-    $("#drink-info").empty()
+    $("#drink-info").empty();
    
     $.ajax({
         url: url,
         method: "GET"
     }).then(function(response) {
         let drinkDiv = $("<div>");
-        let drink = response.drinks[0]
-        let drinkName = $("<h3>").text(drink.strDrink);
-        let drinkPic = $("<img>")
+        let drink = response.drinks[0];
+        let drinkName = $("<h2>").text(drink.strDrink);
+        let drinkPic = $("<img>");
         let drinkPicImg = drink.strDrinkThumb;
         let ingDiv = $("<div>");
+        //Ben build this for loop that displays ingredients and amounts
         for( var i = 1; i < 16; i++){
             let ing = 'strIngredient' + [i];
             let ingAmt = 'strMeasure' + [i];
-            let currAmt = drink[ingAmt]
+            if(ingAmt === null){
+                ingAmt = ""
+            };
+            let currAmt = drink[ingAmt];
             let currIng = $("<p>" + drink[ing] + ": " + currAmt + "</p>");
 
             if(drink[ing] === null){
                 break;
             }else{
                 ingDiv.append(currIng)
-            }
-
-            console.log(ing)
-            console.log(currIng)
-        }
-        drinkPic.attr("src", drinkPicImg)
-        drinkPic.attr("class", "is-rounded mt-3")
+            };
+        };
+        drinkPic.attr("src", drinkPicImg);
+        drinkPic.attr("class", "is-rounded mt-3");
         let instructions = drink.strInstructions;       
         drinkDiv.append(drinkName, drinkPic, ingDiv, instructions);
         $("#drink-info").append(drinkDiv);
-    })    
-})
+    });
+});
 
 
-
+//here is the second API call. Ray figured out how to format the url, and the info that returns.
 $("#recipe-button").on("click", function(event){
-    event.preventDefault()
+    event.preventDefault();
     $("#recipeDiv").empty();
     var searchQ = $(".input").val()
     var maxCookTime = $("#cook-time select").val()
@@ -58,9 +59,11 @@ $("#recipe-button").on("click", function(event){
     }).then(function(response){
         console.log(queryURL)
         console.log(response)
+        //Ben added the randomization or the recipe
         let j = Math.ceil(Math.random() * 30)
         let recipe = response.hits[j].recipe;
         console.log(j)
+        //both Ray and Ben build the appending system for the recipe
         let recipeCard = $("<div>");
         let title = $('<p>').text(recipe.label);
         title.attr('class', "title is-4")
@@ -68,6 +71,7 @@ $("#recipe-button").on("click", function(event){
         let cookTime = $('<p>').text(recipe.totalTime + " minutes");
         let recipeLink = $("<a>" + "Click Here For the Recipe!" + "</a>");
         recipeLink.attr("href", recipe.url)
+        recipeLink.attr("target", "_blank")
         recipeImg.attr('src', recipe.image);
         recipeCard.attr('class', "recipe-card");
         recipeCard.append(title);
